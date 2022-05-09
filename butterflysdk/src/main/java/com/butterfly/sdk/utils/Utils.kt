fb0e984913a79fee819ce634b2b3d8c2d69f5840
Perry <perrchick@gmail.com>
2022-05-09 11:11:16 +0300

    v1.1.4

--- butterflysdk/src/main/java/com/butterfly/sdk/utils/Utils.kt
+++ butterflysdk/src/main/java/com/butterfly/sdk/utils/Utils.kt
@@ -7,7 +7,7 @@ import java.lang.ref.WeakReference
 
 class Utils {
     companion object {
-        const val BUTTERFLY_SDK_VERSION: String = "1.1.3"
+        const val BUTTERFLY_SDK_VERSION: String = "1.1.4"
 
         var isDebuggable: Boolean? = null
         var applicationContextWeakReference: WeakReference<Context>? = null
