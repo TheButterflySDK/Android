269237e2ec63f2337a90714652e68359fb7e5744
perrchick <perrchick@gmail.com>
2025-09-02 21:07:12 +0300

    v2.2.0 - add examples for new APIs

--- app/src/main/java/com/example/butterflyhost/MainActivity.java
+++ app/src/main/java/com/example/butterflyhost/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         final Activity activity = this;
 
         button.setOnClickListener(v ->
-                ButterflySdk.openReporter(activity, API_KEY)
+                ButterflySdk.open(activity, API_KEY)
         );
 
         boolean useDarkIcons = true; // dark icons = light status bar background
@@ -43,11 +43,15 @@ public class MainActivity extends AppCompatActivity {
         controller.setAppearanceLightNavigationBars(useDarkIcons); // requires API 26+
         
         // Handle deep link if app was launched from a URL (cold start)
+        ButterflySdk.handleIncomingIntent(this, getIntent(), API_KEY);
+        // OR:
+        ButterflySdk.handleDeepLink(this, "https:www.your-website.com/path?param=value", API_KEY);
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         // Handle deep link if app was already running (warm start)
+        ButterflySdk.handleIncomingIntent(this, intent, API_KEY);
     }
 }
